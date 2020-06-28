1ec1e983cb33d7ed9a0e11215f9890e1ffa95aad
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-28 21:26:22 +0000

    Automatically backed up by Learn

--- js/reducer.js
+++ js/reducer.js
@@ -1,1 +1,35 @@
 // add code snippets from README
+
+
+
+
+
+---
+
+export let state;
+
+
+export function managePets(state = { pets: []}, action){
+ switch (action.type) {
+      case 'ADD_PET':
+        return { pets: [...state.pets, action.pet] };
+      case 'REMOVE_PET':
+        return { pets: state.pets.filter(pet => pet.id !== action.id) };
+      default:
+        return state;
+  }
+}
+
+export function dispatch(action){
+  state = managePets(state, action);
+  render();
+}
+
+
+export const render = () => {
+  let container = document.getElementById('container');
+  let petsList = state.pets.map((pet) => {
+    return `<li>${pet.name}</li>`;
+  }).join(' ');
+  container.innerHTML = `<ul>${petsList}</ul>`;
+}
